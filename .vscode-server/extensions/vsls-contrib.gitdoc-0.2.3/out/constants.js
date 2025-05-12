a569366f9766ae192e38c2251a6db3add5cad9d4
Ubuntu <ubuntu@ip-172-31-33-191.us-west-2.compute.internal>
2025-05-12 22:51:16 +0000

    Remove .vscode-server and add to .gitignore

--- .vscode-server/extensions/vsls-contrib.gitdoc-0.2.3/out/constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.EXTENSION_NAME = void 0;
-exports.EXTENSION_NAME = "gitdoc";
-//# sourceMappingURL=constants.js.map
